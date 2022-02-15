05ae8057f4c881150a5767be4a93d09c87d5723c
Enrique Alejandro Galván Montes <enrique.agm2177@gmail.com>
2022-02-15 01:54:54 -0600

    Terminado Kata 3

---
+++ Kata_3/kata3.py
@@ -0,0 +1,23 @@
+#Ejercicio 1
+veloAsteroide = 49
+if veloAsteroide > 25:
+  print("VIENE UN ASTEROIDEEEEE")
+else:
+  print("No pasa nada")
+#Ejercicio 2
+veloAsteroide2 = 19
+if veloAsteroide2 >= 20:
+  print("A buscar un asteroide en el cielooo")
+else:
+  print("No pasa nada")
+#Ejercicio 3
+veloAsteroide3 = 49
+tamanioAsteroide = 30
+if veloAsteroide3 > 25 and tamanioAsteroide > 25:
+  print("VIENE UN ASTEROIDEEEEEEEEE")
+elif veloAsteroide3 >= 20:
+  print("A buscar un asteroide en el cieloo")
+elif tamanioAsteroide < 25:
+  print("No pasa nada")
+else:
+  print("No pasa nada")
